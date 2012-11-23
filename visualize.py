1d305d532d50ea697f0bc6963926cfc0ea560ba8
aaron <jackskag@gmail.com>
2012-11-23 17:56:32 -0500

    improved visualization alogorithm

--- visualize.py
+++ visualize.py
@@ -15,16 +15,27 @@ def createGuageObject(scores):
   current_week = scores[0].week_date
   weeks = dict()
   entries_per_week, sum_of_pr, sum_of_cm, sum_of_ex, sum_of_ch, sum_of_pl = 0,0,0,0,0,0
+  store_op, reset_op, iterate_op = False, False, True 
 
   for score in scores:
 
     pr, cm, ex, ch, pl = score.pride, score.communication, score.expectations, score.challenge, score.pulse
-    logging.warning(scores.index(score))
-    logging.warning(len(scores)-1)
-    logging.warning(scores.index(score) == len(scores) - 1)
+    
+    if score.week_date != current_week: ## if state is start of new week then don't iterate, store and reset
+      iterate_op, store_op, reset_op = False, True, True
 
-    if score.week_date != current_week or scores.index(score) == len(scores) - 1:
-      logging.warning('elif')
+    if scores.index(score) == len(scores) - 1: ## if state is last object then interate and store, but don't reset
+      interate_op, store_op, reset_op = True, True, False
+
+    if iterate_op: ## iterate operation
+      entries_per_week += 1  
+      sum_of_pr += pr
+      sum_of_cm += cm
+      sum_of_ex += ex
+      sum_of_ch += ch
+      sum_of_pl += pl
+
+    if store_op: ## store operation
       weeks[current_week] = dict()
       weeks[current_week]['entries_per_week'] = entries_per_week
       weeks[current_week]['pr'] = sum_of_pr
@@ -33,21 +44,11 @@ def createGuageObject(scores):
       weeks[current_week]['ch'] = sum_of_ch
       weeks[current_week]['pl'] = sum_of_pl
 
-      current_week = score.week_date
-      entries_per_week = 1
-      sum_of_pr = pr
-      sum_of_cm = cm
-      sum_of_ex = ex
-      sum_of_ch = ch
-      sum_of_pl = pl
-
-    elif score.week_date == current_week:
-      entries_per_week += 1  
-      sum_of_pr += pr
-      sum_of_cm += cm
-      sum_of_ex += ex
-      sum_of_ch += ch
-      sum_of_pl += pl
+    if reset_op: ## reset operation
+      iterate_op, store_op, reset_op = True, False, False ## just iterate the next iterable
+      current_week = score.week_date ## reset current_week
+      entries_per_week = 1 ## reset entries_per_week
+      sum_of_pr, sum_of_cm, sum_of_ex, sum_of_ch, sum_of_pl = pr, cm, ex, ch, pl
 
   logging.warning(weeks)
 
